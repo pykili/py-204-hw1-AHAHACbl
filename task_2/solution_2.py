59dc2c6eee8c04de8179c7d5c8b43b3db43293ac
AHAHACbl <72072247+AHAHACbl@users.noreply.github.com>
2020-10-30 02:00:49 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,12 @@
-# your code here
+l=0
+m=0
 user_input = input()
-# your code here
+a = len(user_input)
+for i in range(0,a):
+  for j in range(0,a):
+    if user_input[i]==user_input[j]:
+      l=l+1
+      if l>m:
+        m=l
+        most_frequent_character = user_input[i]
 print(most_frequent_character)
