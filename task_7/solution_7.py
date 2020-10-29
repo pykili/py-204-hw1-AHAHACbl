91eec72585dee3d8482cf644d03c19b6a51052d0
AHAHACbl <72072247+AHAHACbl@users.noreply.github.com>
2020-10-29 21:42:08 +0300

    Update solution_7.py

--- task_7/solution_7.py
+++ task_7/solution_7.py
@@ -1,1 +1,9 @@
-# your code here
+a=""
+string = input()
+num = len(string)
+for i in range(0, num):
+  a=(a + string[num-i-1])
+if a==string:
+  is_palindrom=True
+else: is_palindrom=False
+print(is_palindrom)
