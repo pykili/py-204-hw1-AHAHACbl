8424c2835462cdc95b3ca7a16ea10888c6728bbb
AHAHACbl <72072247+AHAHACbl@users.noreply.github.com>
2020-10-29 20:53:05 +0300

    Update solution_3_2.py

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -1,6 +1,9 @@
-# your code here
-for smth in 'a'*10:
-    # your code here
-    user_input = input()
-# your code here
+alphabet = ""
+user_input = ""
+for i in range (1,11):
+  line = input()
+  user_input = (user_input + line)
+for i in user_input:
+  if i not in alphabet:
+    alphabet = (alphabet + i)
 print(alphabet)
